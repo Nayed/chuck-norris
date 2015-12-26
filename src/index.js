e7cd715fe7d4fe24393fa92e87453b97e7d0de1a
Nayed SAID ALI <nayed.saidali@gmail.com>
2015-12-26 16:19:43 +0100

    persistance log in/out

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import Home from './components/Home.vue'
 import SecretQuote from './components/SecretQuote.vue'
 import Signup from './components/Signup.vue'
 import Login from './components/Login.vue'
+import auth from './auth'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 
 Vue.use(VueResource)
 Vue.use(VueRouter)
+
+// Check the user's auth status when the app starts
+auth.checkAuth()
 
 export let router = new VueRouter()
 
